321f8f7aeae75ee8da71ee5454e63d26cb3a941c
RenatoDardengo <rdardengo01@gmail.com>
2022-07-03 21:38:16 -0300

    Concluido configuração do entry-point

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require ("express");
 const app = express();
 const port = 3000;
+const methodOverride = require ("method-override");
+const addressRoute = require("./src/routes/addressRoute");
+
+app.use(express.static(__dirname + "/src/public"));
+app.use(methodOverride("_method"));
+app.use(express.json);
+app.use(express.urlencoded({extended:false}));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/src/views");
+
+app.use("/address", addressRoute);
+
+
 
 app.listen(port, ()=>{
     console.log (`Servidor rodando em http://http://localhost:${port}`)
-})
+});
